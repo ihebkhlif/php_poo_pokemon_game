fb8bbe8f9dcec39710cd2d21b4a6af61268bdc67
mohamedACHOURI <mahometashoury@gmail.com>
2025-04-08 21:58:05 +0100

    fin

--- index.php
+++ index.php
@@ -176,3 +176,9 @@ if (isset($_POST['reset'])) { ?>
         });
         document.querySelector('.lobby_audio').volume = 0.2;
     </script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
