08034e94825776ddc8374cc652271ffd3c825e8a
mohamedACHOURI <mahometashoury@gmail.com>
2025-04-08 21:30:35 +0100

    construction de la classe pokemon et definition des methodes basiques

--- pokemon_class.php
+++ pokemon_class.php
@@ -14,4 +14,71 @@ class AttackPokemon
     }
 }
 
+class pokemon
+{
+    public string $name;
+    public string $image;
+    public int $maxHP;
+    public int $HP;
+    public AttackPokemon $attackPokemon;
+    public function __construct($name, $image, $HP, AttackPokemon $attackPokemon)
+    {
+        $this->name = $name;
+        $this->image = $image;
+        $this->HP = $HP;
+        $this->maxHP = $HP;
+        $this->attackPokemon = $attackPokemon;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function getHP(): int
+    {
+        return $this->HP;
+    }
+
+    public function setHP(int $HP): void
+    {
+        $this->HP = $HP;
+    }
+
+    public function getAttackPokemon(): AttackPokemon
+    {
+        return $this->attackPokemon;
+    }
+
+    public function setAttackPokemon(AttackPokemon $attackPokemon): void
+    {
+        $this->attackPokemon = $attackPokemon;
+    }
+    public function IsDead(): bool
+    {
+        return $this->HP <= 0;
+    }
+
+
+
+
+
+
+}
+
+
 
